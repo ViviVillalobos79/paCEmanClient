1162025ca558ef4384583a416c3f3ab9fb6d8848
Dxnium <38996231+Dxnium@users.noreply.github.com>
2020-08-02 15:42:02 -0600

    server-java JSON

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import Sockets.Cliente;
+
+import java.io.*;
+import java.net.Socket;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+    public static void main(String[] args) throws IOException {
+        Cliente c = new Cliente();
     }
 }
